311a75d3fe64ad0150965c49f10a05918759f772
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-07-06 23:54:45 -0300

    ve

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 /** Configure PORT to connect client with the server */
 
 const PORT = () => {
-  return ''
+  return 'https://black-wave-0a8e8fc10.1.azurestaticapps.net/'
 }
 
 export default PORT
